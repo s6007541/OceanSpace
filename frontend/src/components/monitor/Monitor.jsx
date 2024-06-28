cb765e582d959c89dca475802c264920fb3220de
Sorn Chottananurak <sorn111930@gmail.com>
2024-06-28 16:51:59 +0700

    update custom and chat

--- frontend/src/components/monitor/Monitor.jsx
+++ frontend/src/components/monitor/Monitor.jsx
@@ -1,14 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "./monitor.css";
 import EmojiPicker from "emoji-picker-react";
-import {
-  arrayUnion,
-  doc,
-  getDoc,
-  onSnapshot,
-  updateDoc,
-} from "firebase/firestore";
-import { db } from "../../lib/firebase";
 import { useChatStore } from "../../lib/chatStore";
 import { useUserStore } from "../../lib/userStore";
 import upload from "../../lib/upload";
