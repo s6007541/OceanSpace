db6395b65b9020eada332f2a38c6a9f48a708286
Sorn Chottananurak <sorn111930@gmail.com>
2024-06-28 15:01:37 +0700

    update add friend page

--- frontend/src/components/addfriend/Addfriend.jsx
+++ frontend/src/components/addfriend/Addfriend.jsx
@@ -1,26 +1,63 @@
-import { useEffect, useRef, useState } from "react";
 import "./addfriend.css";
-import EmojiPicker from "emoji-picker-react";
-import {
-  arrayUnion,
-  doc,
-  getDoc,
-  onSnapshot,
-  updateDoc,
-} from "firebase/firestore";
-import { db } from "../../lib/firebase";
-import { useChatStore } from "../../lib/chatStore";
+import { useState } from "react";
 import { useUserStore } from "../../lib/userStore";
-import upload from "../../lib/upload";
-import { format } from "timeago.js";
+import { LLM_DICT, LLM_LIST } from "../../lib/llm_lists"
+import { useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
-const Addfriend = () => {
+const AddFriend = ( ) => {
+  
+  const navigate = useNavigate(); 
+  const goback = () =>{ 
+    let path = `/`; 
+    navigate(path);
+  }
+  const gocustom = () =>{ 
+    let path = `/Custom`; 
+    navigate(path);
+  }
 
+  const goLLMProfile = async (LLM_info) => {
+    navigate("/llmprofile", {state : [LLM_info, location.state]});
+  };
+
+
+  const { currentUser } = useUserStore();
+
+  const handleAddLLM = async (LLMId) => {
+  };
+
+
+  
   return (
-    <div className="chat">
-     Addfriend
+    <div className="addFriend">
+      <img className="goback" src="./arrowLeft.svg" onClick={goback}/>
+      <div className="header_text">
+      รายชื่อเพื่อนทั้งหมด
+      </div>
+      <div className="list_char">
+        <button className="item_add" onClick={gocustom}>+ สร้างคาแรกเตอร์สัตว์น้ำของตัวเอง</button>
+        {LLM_LIST.map((llm) => (
+          <div className="item" onClick={()=>{goLLMProfile(LLM_DICT[llm])}}>
+            <img
+              src={`./SeaCharacters/Small-56px/${LLM_DICT[llm].avatar}`}
+              className="llm_button"
+              id={llm}
+              alt=""
+            />
+            <div className="llm_text" id={llm}>
+              <div className="username">{LLM_DICT[llm].username}</div>
+              <div className="desc">{LLM_DICT[llm].description}</div>
+            </div>
+            <button className="button-add">
+             เพิ่มแล้ว
+            </button>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 };
 
-export default Addfriend;
+export default AddFriend;
---
+++ frontend/src/lib/llm_lists.js
@@ -0,0 +1,5 @@
+import llm_dict from '../../../llm_config.json';
+
+export const LLM_DICT = llm_dict
+
+export const LLM_LIST = ["IamBlueWhale", "IamPinkDolphin"];
