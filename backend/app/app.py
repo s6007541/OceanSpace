8a22376a99998cf35c46d05fa3f9a4d970137eb6
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-06-30 01:52:12 +0700

    implement wss

--- backend/app/app.py
+++ backend/app/app.py
@@ -67,6 +67,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     f"http://{get_local_ip_address()}:5173",
+    "https://ocean-space.onrender.com",
 ]
 
 app = FastAPI(lifespan=lifespan)
--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,2 +1,5 @@
-export const BACKEND_URL = `http://${window.location.hostname}:8000`;
-export const WEBSOCKET_URL = `ws://${window.location.hostname}:8000`;
+// export const BACKEND_URL = `http://${window.location.hostname}:8000`;
+// export const WEBSOCKET_URL = `ws://${window.location.hostname}:8000`;
+export const BACKEND_URL = `https://ocean-space-backend.onrender.com`;
+export const WEBSOCKET_URL = `wss://ocean-space-backend.onrender.com`;
+
