d4e5016d548671f7651be472ac5aa66688a51cd9
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-07-04 15:29:30 +0700

    remove comment

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -8,7 +8,6 @@ export const useSocket = create((set) => ({
     const token = localStorage.getItem("token");
     if (token) {
       const ws = new WebSocket(url);
-      // const ws = new WebSocket(url, ["Authorization", token]);
       set({ socketConnected: true });
       ws.addEventListener("open", (_) => {
         set({ socket: ws });
