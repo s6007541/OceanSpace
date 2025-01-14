507fd00039cfc0cb1d64ef3890363a87b98381c7
Noppanat Wadlom <noppanat.wad@gmail.com>
2025-01-15 04:42:40 +0800

    change setup script for render

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,5 @@ source .env
 
 set -e
 
-ngrok http --url=$BASE_URL http://127.0.0.1:8000 > /dev/null &
-
 cd backend
 python main.py
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,16 +3,6 @@ source .env
 
 set -e
 
-# Check if ngrok is installed
-if ! command -v ngrok 2>&1 >/dev/null
-then
-    echo "ngrok could not be found. Please install it from https://download.ngrok.com."
-    exit 1
-fi
-
-# Add ngrok's auth token
-ngrok config add-authtoken $NGROK_AUTH_TOKEN
-
 # Install frontend dependencies
 cd frontend
 npm install
