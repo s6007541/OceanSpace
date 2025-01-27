5aff3bfe8d4501dd0d8ab427e24eb2948048de18
Sorn Chottananurak <sorn111930@gmail.com>
2025-01-28 00:06:44 +0700

    update

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", log_level="info")
+    # uvicorn.run("app.app:app", log_level="info")
+    uvicorn.run("app.app:app", host="0.0.0.0", port=80, log_level="info")
+
