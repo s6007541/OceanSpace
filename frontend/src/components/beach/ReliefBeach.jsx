5d36bf44c651b625e71df59fa08c89cde699e9f2
Sorn Chottananurak <sorn111930@gmail.com>
2025-01-30 05:23:39 +0700

    update wish beach

--- frontend/src/components/beach/ReliefBeach.jsx
+++ frontend/src/components/beach/ReliefBeach.jsx
@@ -68,14 +68,14 @@ const ReliefBeach = () => {
   const getAnimation1 = () => {
     // if (blurLevel === 0) return {};
     console.log(blurLevel);
-    const animation = (blurLevel > 0) ? "seafoam11 6s infinite" : "seafoam1 5s infinite";
+    const animation = (blurLevel > 0) ? "seafoam11 6s infinite" : "seafoam1 6s infinite";
     return { animation };
   };
 
   const getAnimation2 = () => {
     // if (blurLevel === 0) return {};
     console.log(blurLevel);
-    const animation = (blurLevel > 0) ? "seafoam22 6s infinite" : "seafoam2 5s infinite";
+    const animation = (blurLevel > 0) ? "seafoam22 6s infinite" : "seafoam2 6s infinite";
     return { animation };
   };
   
