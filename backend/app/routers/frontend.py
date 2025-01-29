b201e69026fbf6e812ae8389f122e6a3f2fe5888
Noppanat Wadlom <noppanat.wad@gmail.com>
2025-01-29 21:15:51 +0800

    remove unnecessary logging

--- backend/app/routers/frontend.py
+++ backend/app/routers/frontend.py
@@ -7,5 +7,4 @@ router = APIRouter()
 
 @router.get("/{other_path:path}")
 async def serve_react_app(other_path: str):
-    print("serve react app", other_path)
     return FileResponse("../frontend/dist/index.html")
