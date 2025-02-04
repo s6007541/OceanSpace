34c5377b01c585368f9891f8136b434991c60514
Sorn Chottananurak <sorn111930@gmail.com>
2025-02-05 03:48:40 +0700

    revert to old main

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,6 @@
 import uvicorn
-import argparse
-
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--local_dev", action="store_true", help="Run tests locally")
-
-
-    args = parser.parse_args()
     
-    if args.local_dev:
-        uvicorn.run("app.app:app", host="0.0.0.0", port=80, log_level="info")   # For local development
-    else:
-        uvicorn.run("app.app:app", log_level="info")
+    # uvicorn.run("app.app:app", host="0.0.0.0", port=80, log_level="info")   # For local development
+    uvicorn.run("app.app:app", log_level="info")
--- scripts/update_frontend.sh
+++ scripts/update_frontend.sh
@@ -12,4 +12,4 @@ set -e
 # ngrok http --url=$BASE_URL http://127.0.0.1:8000 > /dev/null &
 
 cd backend
-python main.py --local_dev
+python main.py
